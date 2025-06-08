80218683ac37d5ec6355df0b018431b322045aa2
kromer52 <ilonakromer52@googlemail.com>
2025-06-08 10:38:03 +0200

    updated html & js files
    Updating footer, updating prompt/context, updating html text & functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayAttractions(response) {
+  new Typewriter("#attractions", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -7,21 +7,21 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateAttractions(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "303634af30at1e0bobd77c2b1f682f81";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem. Display the poem You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Don't include ``` and html in the poem";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    "Act as an experienced low-budget traveller. Help with attractions that can be visited for free. Provide 3-5 free of charge attractions in the city requested by the user. Display each recommendation in a new line in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Just provide the name of the attractions without any comments. Don't include ``` or html in your answer.";
+  let prompt = `User instructions: Generate 3-5 attractions free of charge in the city provided ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  let attractionsElement = document.querySelector("#attractions");
+  attractionsElement.classList.remove("hidden");
+  attractionsElement.innerHTML = `<div class="generating">⏳ Generating free attractions in ${instructionsInput.value}</div>`;
   
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayAttractions);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let attractionsFormElement = document.querySelector("#attractions-generator-form");
+attractionsFormElement.addEventListener("submit", generateAttractions);
